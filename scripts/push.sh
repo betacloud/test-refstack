0e8d5c0f2969f2852b62b8359e77341ceb03b73d
Christian Berendt <berendt@betacloud-solutions.de>
2019-04-24 23:05:27 +0200

    Make push to refstack.openstack.org configurable

--- scripts/push.sh
+++ scripts/push.sh
@@ -3,4 +3,8 @@
 source refstack-client/.venv/bin/activate
 
 resultfile=$(find refstack-client/.tempest/.stestr -name 'betacloud-*.json' | sort -r | head -n1)
-refstack-client upload $resultfile --url https://refstack.openstack.org/api -i id_rsa.refstack -y
+
+PUSH_TO_REFSTACK=${PUSH_TO_REFSTACK:-false}
+if [[ $PUSH_TO_REFSTACK == "true" ]]; then
+    refstack-client upload $resultfile --url https://refstack.openstack.org/api -i id_rsa.refstack -y
+fi
